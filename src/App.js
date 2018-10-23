771e8b8df90cc5b3c6dd760a055b299ec1036c45
kirbubu <38482025+kirbubu@users.noreply.github.com>
2018-10-23 13:02:29 -0700

    Information update v1
    Added some additional information to the website in the form of paragraphs and headers. Information seen on main page.

--- src/App.js
+++ src/App.js
@@ -51,7 +51,27 @@ class App extends Component {
             losing concentration, or skipping over words.
           </p>
           <p>
-            <Title title='What should I put here?'/>
+            <Title title='See how it works.'/>
+            <p>Swiftionary works in a simple fashion to simplify trivial searches. No typing,
+              no search bars, just instant results.
+            </p>
+            <p>With a plethora of features for users to explore, Swiftionary
+                allows you to learn different things about the unknown in different
+                ways.
+            </p>
+            
+          </p>
+          <p>
+            <Title title='Choose what you want.'/>
+            <p>Don't like highlighting? Right click on the word to select options.
+              Don't like all the options Swifitonary provides? Choose which ones you 
+              like the best.
+            </p>
+            <p>
+              Choose what features you've been looking for when you initally download
+              Swiftionary. If you grow tired of one, or are looking for something new,
+              you can always change it up in the settings.
+            </p>
           </p>
           <a
             className="App-link"
