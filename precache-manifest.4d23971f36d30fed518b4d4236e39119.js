d10d16cdcb5401569e6644bd2bc3bd155ed338a9
kirbubu <philpoppler@gmail.com>
2018-11-05 11:26:11 -0800

    Updates

---
+++ precache-manifest.4d23971f36d30fed518b4d4236e39119.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/sw/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "46cabe8d64535790cf3a",
+    "url": "/sw/static/js/runtime~main.46cabe8d.js"
+  },
+  {
+    "revision": "97043d60089ed2a6d7c8",
+    "url": "/sw/static/js/main.97043d60.chunk.js"
+  },
+  {
+    "revision": "51f0c74e76d1ab14a06a",
+    "url": "/sw/static/js/1.51f0c74e.chunk.js"
+  },
+  {
+    "revision": "97043d60089ed2a6d7c8",
+    "url": "/sw/static/css/main.6a7b0503.chunk.css"
+  },
+  {
+    "revision": "12c5ee6c0d787fb25ef16b4007e9e0bd",
+    "url": "/sw/index.html"
+  }
+];
