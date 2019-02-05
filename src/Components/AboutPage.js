45c977379ac1d051498e45d73d9a1ace0b20b642
IntangibleTM <33038600+IntangibleTM@users.noreply.github.com>
2019-02-04 16:19:05 -0800

    changed image and format in aboutpage
    yes

--- src/Components/AboutPage.js
+++ src/Components/AboutPage.js
@@ -14,20 +14,15 @@ class AboutPage extends Component{
     <div>
     <link href="https://fonts.googleapis.com/css?family=BioRhyme" rel="stylesheet"></link>
     <link href="https://fonts.googleapis.com/css?family=Source+Serif+Pro" rel="stylesheet"></link>
-<h1 className ="aboutTitle"><center>About</center>
-<img src ="csulbTransparent.png" width="500" height ="500" className="csulbLogo" alt="logo"></img>
+<h1 className ="aboutTitle">About
 </h1>
 <p></p>
 <p className ="aboutPara1"> 
-
-{/* 
-<img src ="SealGoldBlk.jpg" width="500" height ="500" className="csulbLogo" ></img>
-*/}
 Swiftionary was created in August, 2018. Four friends discussed how they hated 
 having to open another tab to search for the meanings of words when browsing the internet.
 They came with the brilliant idea of creating a versatile Google Chrome Web Extension that would solve this problem. Web extensions are a fast way 
 to enhance the web browsing experience and that is why they decided to make this project as a web extension.</p>  
-
+<img src ="Swiftionary Logo.png" width="200" height ="200" className="csulbLogo" alt="logo"></img>
 </div>  
 
 </div>
